93a26c93dfb59084d3c7e4a5c94ee7351431af9a
David Whipple <dwhipp88@gmail.com>
2024-05-20 14:02:59 -0600

    added project files, replaced lesson files with project files

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// let generateName = require('sillyname');
-// import generateName from 'sillyname';
-// let sillyName = generateName();
-
-// console.log(`My name is ${sillyName}`);
-
-import randomSuperHero from 'sillyname';
-
-let superHeroName = randomSuperHero();
-
-console.log(`I am ${superHeroName}!`);
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
