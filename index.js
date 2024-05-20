e9445c69d6268a642286904fd4ecabfca8d59a05
David Whipple <dwhipp88@gmail.com>
2024-05-20 15:04:00 -0600

    added dependencies, setup inquirer

--- index.js
+++ index.js
@@ -3,3 +3,34 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import inquirer from 'inquirer';
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'inputURL',
+            message: 'Enter URL here (Required): ',
+            validate: (urlInput) => {
+                if (urlInput) {
+                    return true;
+                } else {
+                    console.log('Enter URL here (Required): ');
+                    return false;
+                }
+            },
+        },
+    ])
+    .then((data) => {
+        console.log('Data: ', data);
+    })
+    .catch((error) => {
+        if (error) {
+            // Promp couldn't be rendered in the current environment
+            console.log('Error: ', error);
+        } else {
+            // Something else went wrong
+            console.log('Success!');
+        }
+    });
